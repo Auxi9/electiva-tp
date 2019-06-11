8a9a6da6c14b236ded2d1cd38d2731fe299c2639
juanrjarolin <juanjarolin994@gmail.com>
2019-06-11 19:32:06 -0400

    agregando localstorage

--- src/app/index.js
+++ src/app/index.js
@@ -4,8 +4,12 @@ require('./index.css');
 
 const { Weather } = require('./weather'); //importando la clase Weather
 const { UI } = require('./ui'); //importando la clase UI
+const { Store } = require('./store');
 
-const weather = new Weather('Itá', 'py');
+const store = new Store();
+const { ciudad, codigo } = store.getLocationData();
+
+const weather = new Weather(ciudad, codigo);
 const ui = new UI();
 
 //se ejecutará cuando el contenido haya sido cargado
@@ -18,6 +22,7 @@ document.getElementById('w-change-btn').addEventListener('click', (e) => {
     const ciudad = document.getElementById('city').value;
     const codigo = document.getElementById('countryCode').value;
     weather.cambioLugar(ciudad, codigo);
+    store.setLocationData(ciudad, codigo); //se almacenará los datos en el navegador
     obtenerClima();
     e.preventDefault();
 });
--- src/app/store.js
+++ src/app/store.js
@@ -4,7 +4,33 @@ export class Store {
     constructor(){
         this.ciudad;
         this.codigo;
-        this.defaultCiudad = 'London';
-        this.defaultCodigo = 'uk';
+        this.defaultCiudad = 'Itá';
+        this.defaultCodigo = 'py';
+    }
+
+    getLocationData(){
+        //muestra lo que el navegador ha almacenado
+        if(localStorage.getItem('ciudad') === null){
+            this.ciudad = this.defaultCiudad; //si no existe una ciudad almacenada, establece una ciudad por defecto
+        }else{
+            this.ciudad = localStorage.getItem('ciudad'); //sino establece lo q está almacenado
+        }
+
+        if(localStorage.getItem('codigo') === null){
+            this.codigo = this.defaultCodigo; //si no existe un codigo almacenado, establece una codigo por defecto
+        }else{
+            this.codigo = localStorage.getItem('codigo'); //sino establece lo q está almacenado
+        }
+        
+        return {
+            ciudad: this.ciudad,
+            codigo: this.codigo
+        }
+    }
+
+    setLocationData(ciudad, codigo){
+        //almacenando en localstorage del navegador
+        localStorage.setItem('ciudad', ciudad);
+        localStorage.setItem('codigo', codigo);
     }
 }
