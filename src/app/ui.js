0fb47039cbf4ed3f669ea3b1806642eb723128a7
Auxi Quevedo <quevedoauxi@gmail.com>
2019-06-11 23:28:23 -0400

    Mejoras en render

--- src/app/ui.js
+++ src/app/ui.js
@@ -6,6 +6,10 @@ export class UI {
         this.temperatura = document.getElementById('weather-string');
         this.humedad = document.getElementById('weather-humidity'); //humedad
         this.viento = document.getElementById('weather-wind'); //viento
+        // SE AGREGARON LOS ELEMENTOS: ICONO, TEMP MAX, TEMP MIN
+        this.icon = document.getElementById('weather-icon');
+        this.min = document.getElementById('min-string');
+        this.max = document.getElementById('max-string');
     }
 
     render(weather){
@@ -15,5 +19,8 @@ export class UI {
         this.temperatura.textContent = weather.main.temp + ' °C';
         this.humedad.textContent = 'Humedad: ' + weather.main.humidity + '%';
         this.viento.textContent = 'Viento: ' + weather.wind.speed + 'm/s';
+        this.icon.src = "http://openweathermap.org/img/w/" + weather.weather[0]['icon'] + ".png"
+        this.min.textContent = 'Mínima: ' + weather.main.temp_max + ' °C';
+        this.max.textContent = 'Máxima: ' + weather.main.temp_max + ' °C';
     }
 }
